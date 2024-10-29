4863cf9eff7da9559d2c814f5e4d432a0be45657
Lungu Mihai Adrian <adrian.lungu@csie.ase.ro>
2024-10-29 17:36:43 +0200

    seminar 5

---
+++ seminar5/dom.js
@@ -0,0 +1,15 @@
+const incrementBtn = document.getElementById('incrementBtn');
+const decrementBtn = document.getElementById('decrementBtn');
+const spanCounter = document.getElementById('counter');
+
+let counter = 0;
+
+incrementBtn.addEventListener('click', () => {
+    counter++;
+    spanCounter.textContent = counter;
+})
+
+decrementBtn.addEventListener('click', () => {
+    counter--;
+    spanCounter.textContent = counter;
+})
---
+++ seminar5/setTimeOut.js
@@ -0,0 +1,7 @@
+console.log(1)
+
+setTimeout(() => {
+    console.log('set time out')
+}, 1000)
+
+console.log(2)
