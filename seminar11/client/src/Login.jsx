75e126682f6d91bbbf231e406d35a763b06a921b
Mihai Adrian Lungu <adrian.lungu@csie.ase.ro>
2024-12-10 17:18:19 +0200

    seminar 11

--- seminar11/client/src/Login.jsx
+++ seminar11/client/src/Login.jsx
@@ -1,6 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(username.length === 0 || password.length === 0) {
+            setError('Empty username or password')
+            return;
+        }
+
+        if (error.length) {
+            setError('')
+        }
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
+        }
+
+        const response = await fetch('http://localhost:3000/auth/login', options);
+        const json = await response.json();
+
+        if (!json.success) {
+            setError(json.message);
+        } else {
+            localStorage.setItem('token', json.data.token);
+            navigate('/');
+        }
+    }
+
     return (
         <div style={{ maxWidth: '400px', margin: '0 auto', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
             <h2>Login</h2>
@@ -40,6 +78,7 @@ const Login = () => {
                 >
                     Save
                 </button>
+                {error.length > 0 && <span>{error}</span>}
             </form>
         </div>
     )
